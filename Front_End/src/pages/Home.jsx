e5bfdddafd4252a3c2b86c8d23c8042b230435c0
Ajit-Aj <ajitaj001@gmail.com>
2025-05-09 09:27:44 +0530

    final commit

--- Front_End/src/pages/Home.jsx
+++ Front_End/src/pages/Home.jsx
@@ -167,14 +167,14 @@ const Home = () => {
             {deleteTaskId && (
                 <div className="fixed inset-0 flex items-center justify-center bg-gray bg-opacity-50 z-50 backdrop-blur-sm">
                     <div className="bg-white p-6 rounded shadow-lg">
-                        <h2 className="text-lg font-semibold">Confirm Deletion</h2>
-                        <p>Are you sure you want to delete this task?</p>
+                        <h2 className="text-lg font-semibold">Delete Task</h2>
+                        <p>Are you sure you want to permanently delete this task?</p>
                         <div className="flex gap-4 mt-4">
                             <button
                                 onClick={handleDelete}
                                 className="bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-700"
                             >
-                                Yes, Delete
+                                Delete
                             </button>
                             <button
                                 onClick={handleCancelDelete}
